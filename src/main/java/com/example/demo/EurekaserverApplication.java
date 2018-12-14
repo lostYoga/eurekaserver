7b6fcfce2a057c8bd39e5a01747ef474e5ab2d22
侯钰佳 <790366648@qq.com>
2018-12-14 16:50:22 +0800

    提交测试

--- src/main/java/com/example/demo/EurekaserverApplication.java
+++ src/main/java/com/example/demo/EurekaserverApplication.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class EurekaserverApplication {
 
 	public static void main(String[] args) {
+		System.out.println("EurekaserverApplication.main");
 		SpringApplication.run(EurekaserverApplication.class, args);
 	}
 }
